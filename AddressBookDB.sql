b5d3b630356175c00c3d981911e781fdf8fa185a
lokeshsonawane95 <lokeshsonawane95@gmail.com>
2022-06-17 11:59:35 +0530

    Added to EditContact

--- AddressBookDB.sql
+++ AddressBookDB.sql
@@ -23,5 +23,10 @@ select * from AddressBook
 
 insert into AddressBook values
 ('Lokesh', 'Sonawane', 'Warje', 'Pune', 'Maharashtra', 411058, 9876543210, 'lokesh.sonawane@gmail.com'),
-('John', 'Connor', 'Warje', 'Pune', 'Maharashtra', 411058, 9876951210, 'john.connor@gmail.com'),
-('Lokesh', 'Sonawane', 'Warje', 'Pune', 'Maharashtra', 411058, 9876543210, 'lokesh.sonawane@gmail.com');
+('John', 'Connor', 'Andheri', 'Mumbai', 'Maharashtra', 400047, 9876951741, 'john.connor@gmail.com'),
+('Mohak', 'Mangal', 'Hennur', 'Bengaluru', 'Karnataka', 560043, 9802587361, 'mohak.mangal@gmail.com');
+
+-- UC4 Edit contacts in AddressBook table
+
+update AddressBook set Phone = 9512364703 where FirstName = 'Mohak';
+
