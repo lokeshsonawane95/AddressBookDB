655735918015c913758d2d38d78c53692515a277
lokeshsonawane95 <lokeshsonawane95@gmail.com>
2022-06-17 11:43:56 +0530

    Added to CreateTable

--- AddressBookDB.sql
+++ AddressBookDB.sql
@@ -5,3 +5,17 @@ create database AddressBookDB;
 
 use AddressBookDB;
 
+-- UC2 Create AddressBook table
+
+create table AddressBook
+(FirstName varchar(50) not null,
+LastName varchar(50) not null,
+Address varchar(50) not null,
+City varchar(50) not null,
+State varchar(50) not null,
+Zip int not null,
+Phone bigint not null,
+Email varchar(50) not null);
+
+select * from AddressBook
+
